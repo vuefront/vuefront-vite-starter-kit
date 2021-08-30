6d07e7ef79dabf62acdf3c67a563b3348ff524aa
Aleksandr Krasnyi <aleksandr.krasnyi@dreamvention.com>
2021-08-30 20:39:40 +0300

    fix: fix bug

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,18 @@ import eslintPlugin from "vite-plugin-eslint";
 import vuefrontPlugin from "vite-plugin-vue-vuefront";
 import voie from "vite-plugin-voie";
 import viteGraphlQl from "vite2-graphql-plugin";
-import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
-    include: ["vuefront"],
+    include: ["vuefront", "vuefront/lib/seo"],
   },
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "/src"),
+      "~": path.resolve(__dirname, "./src"),
     },
   },
   plugins: [
-    // viteCommonjs(),
     vue({
       // skipPreBuild: true,
     }),
