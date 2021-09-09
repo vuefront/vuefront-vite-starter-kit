fb770d23f86caf0a62e33a8b8253fea74e503263
Aleksandr Krasnyi <aleksandr.krasnyi@dreamvention.com>
2021-09-09 14:33:28 +0300

    fix: fix app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,3 @@
 <template>
-  <router-view v-slot="{ Component }">
-    <suspense>
-      <template #default>
-        <component :is="Component" loaded />
-      </template>
-      <template #fallback>
-        <component :is="Component" :loaded="false" />
-      </template>
-    </suspense>
-  </router-view>
+  <vf-t-common-layout />
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "App",
-  mounted() {},
-});
-</script>
